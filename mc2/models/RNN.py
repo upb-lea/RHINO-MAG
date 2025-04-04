84a6d8ec35e09d1fdb3ff583f9bd3415ac3d8433
Hendrik Vater <hvater@mail.uni-paderborn.de>
2025-04-04 07:41:37 +0200

    very basic RNN implementation

---
+++ mc2/models/RNN.py
@@ -0,0 +1,30 @@
+import jax
+import jax.numpy as jnp
+import equinox as eqx
+
+
+class BaseRNN(eqx.Module):
+    """Very basic RNN model."""
+
+    hidden_size: int
+    cell: eqx.Module
+    linear: eqx.nn.Linear
+    bias: jax.Array
+
+    def __init__(self, in_size, out_size, hidden_size, *, key):
+        ckey, lkey = jax.random.split(key)
+        self.hidden_size = hidden_size
+        self.cell = eqx.nn.GRUCell(in_size, hidden_size, key=ckey)
+        self.linear = eqx.nn.Linear(hidden_size, out_size, use_bias=False, key=lkey)
+        self.bias = jnp.zeros(out_size)
+
+    def __call__(self, input):
+        hidden = jnp.zeros((self.hidden_size,))
+
+        def f(carry, inp):
+            rnn_out = self.cell(inp, carry)
+            out = self.linear(rnn_out) + self.bias
+            return rnn_out, out
+
+        _, out = jax.lax.scan(f, hidden, input)
+        return out
