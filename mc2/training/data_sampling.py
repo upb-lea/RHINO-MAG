7c0ef6dd87403162d4bdd389de243b12737cf84d
Hendrik Vater <hvater@mail.uni-paderborn.de>
2025-04-05 00:26:39 +0200

    updated batch sampling to properly draw randomly

--- mc2/training/data_sampling.py
+++ mc2/training/data_sampling.py
@@ -1,8 +1,4 @@
-"""From DataSets to proper batches.
-
-This is a very rough implementation based on code from DMPE.. Really needs a refactoring.
-Especially, the functions can only batch over all sequences in the current dataset -> i.e. you cannot actually set the batch size..
-"""
+"""From DataSets to proper batches."""
 
 import jax
 import jax.numpy as jnp
@@ -10,28 +6,30 @@ import equinox as eqx
 
 
 @eqx.filter_jit
-def precompute_starting_points(n_train_steps, k, sequence_length, training_batch_size, loader_key):
-    index_normalized = jax.random.uniform(loader_key, shape=(n_train_steps, training_batch_size)) * (
-        k + 1 - sequence_length
+def sample_batch_indices(n_sequences, full_sequence_length, sequence_length, training_batch_size, loader_key):
+
+    updated_loader_key, batch_key, starting_point_key = jax.random.split(loader_key, 3)
+
+    n_sequence_indices = jax.random.randint(batch_key, shape=(training_batch_size), minval=0, maxval=n_sequences - 1)
+
+    index_normalized = jax.random.uniform(starting_point_key, shape=(training_batch_size)) * (
+        full_sequence_length + 1 - sequence_length
     )
     starting_points = index_normalized.astype(jnp.int32)
-    (loader_key,) = jax.random.split(loader_key, 1)
 
-    return starting_points, loader_key
+    return n_sequence_indices, starting_points, updated_loader_key
 
 
 @eqx.filter_jit
-def load_single_batch(dataset, starting_points, sequence_length):
+def load_batches(dataset, n_sequence_indices, starting_points, sequence_length):
 
     slice = jnp.linspace(
         start=starting_points, stop=starting_points + sequence_length, num=sequence_length, dtype=int
     ).T
 
-    batched_H = dataset.H[:, slice]
-    batched_B = dataset.B[:, slice]
+    batched_H = dataset.H[n_sequence_indices[..., None], slice]
+    batched_B = dataset.B[n_sequence_indices[..., None], slice]
 
-    batched_H = batched_H[:, :, :]
-    batched_B = batched_B[:, :, :]
     return batched_H, batched_B
 
 
@@ -39,14 +37,12 @@ def load_single_batch(dataset, starting_points, sequence_length):
 def get_data(dataset, sequence_length, training_batch_size, loader_key):
 
     n_sequences, full_sequence_length = dataset.H.shape
-    starting_points, loader_key = precompute_starting_points(
-        1, full_sequence_length, sequence_length, training_batch_size, loader_key
+    n_sequence_indices, starting_points, loader_key = sample_batch_indices(
+        n_sequences, full_sequence_length, sequence_length, training_batch_size, loader_key
     )
-    batched_H, batched_B = load_single_batch(dataset, starting_points, sequence_length)
+    batched_H, batched_B = load_batches(dataset, n_sequence_indices, starting_points, sequence_length)
 
     batched_H = jnp.squeeze(batched_H)[..., None]
     batched_B = jnp.squeeze(batched_B)[..., None]
-
-    # return a batched dataset ?
 
     return batched_H, batched_B, loader_key
