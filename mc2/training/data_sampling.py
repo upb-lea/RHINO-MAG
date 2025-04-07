ff4edacf671f86fa4a7dbadca4cb3256d55262bc
Hendrik Vater <hvater@mail.uni-paderborn.de>
2025-04-07 09:07:44 +0200

    added docstrings to data sampling

--- mc2/training/data_sampling.py
+++ mc2/training/data_sampling.py
@@ -4,16 +4,42 @@ import jax
 import jax.numpy as jnp
 import equinox as eqx
 
+from mc2.data_management import DataSet
+
 
 @eqx.filter_jit
-def sample_batch_indices(n_sequences, full_sequence_length, sequence_length, training_batch_size, loader_key):
+def sample_batch_indices(
+    n_sequences: int,
+    full_sequence_length: jax.Array,
+    training_sequence_length: int,
+    training_batch_size: int,
+    loader_key: jax.Array,
+):
+    """Sample batch indices for the data loader.
+
+    Note: In the current implementation, the full_sequence length can be varied without recompilation of the
+    function. The other parameters are fixed at compile time and cannot be jax.Array as a result. Might make
+    sense to change this in the future.
+
+    Args:
+        n_sequences (int): Number of sequences in the dataset.
+        full_sequence_length (jax.Array): Length of the full sequence.
+        training_sequence_length (int): Length of the training sequence.
+        training_batch_size (int): Size of the training batch.
+        loader_key (jax.Array): Random key for sampling.
+
+    Returns:
+        n_sequence_indices (jax.Array): Indices of the sequences.
+        starting_points (jax.Array): Starting points for the sequences.
+        updated_loader_key (jax.Array): Updated random key.
+    """
 
     updated_loader_key, batch_key, starting_point_key = jax.random.split(loader_key, 3)
 
     n_sequence_indices = jax.random.randint(batch_key, shape=(training_batch_size), minval=0, maxval=n_sequences - 1)
 
     index_normalized = jax.random.uniform(starting_point_key, shape=(training_batch_size)) * (
-        full_sequence_length + 1 - sequence_length
+        full_sequence_length + 1 - training_sequence_length
     )
     starting_points = index_normalized.astype(jnp.int32)
 
@@ -21,10 +47,24 @@ def sample_batch_indices(n_sequences, full_sequence_length, sequence_length, tra
 
 
 @eqx.filter_jit
-def load_batches(dataset, n_sequence_indices, starting_points, sequence_length):
+def load_batches(
+    dataset: DataSet, n_sequence_indices: jax.Array, starting_points: jax.Array, training_sequence_length: int
+):
+    """Load batches of data from the dataset according to the sampled indices.
+
+    Args:
+        dataset (DataSet): Dataset object containing the data.
+        n_sequence_indices (jax.Array): Indices of the sequences.
+        starting_points (jax.Array): Starting points for the sequences.
+        training_sequence_length (int): Length of the training sequence.
+
+    Returns:
+        batched_H (jax.Array): Batched H data.
+        batched_B (jax.Array): Batched B data.
+    """
 
     slice = jnp.linspace(
-        start=starting_points, stop=starting_points + sequence_length, num=sequence_length, dtype=int
+        start=starting_points, stop=starting_points + training_sequence_length, num=training_sequence_length, dtype=int
     ).T
 
     batched_H = dataset.H[n_sequence_indices[..., None], slice]
@@ -34,15 +74,38 @@ def load_batches(dataset, n_sequence_indices, starting_points, sequence_length):
 
 
 @eqx.filter_jit
-def get_data(dataset, sequence_length, training_batch_size, loader_key):
+def draw_data_uniformly(
+    dataset: DataSet, training_sequence_length: int, training_batch_size: int, loader_key: jax.Array
+):
+    """Draw data uniformly from the dataset.
+
+    Args:
+        dataset (DataSet): Dataset object containing the data.
+        training_sequence_length (int): Length of the training sequence.
+        training_batch_size (int): Size of the training batch.
+        loader_key (jax.Array): Random key for sampling.
+
+    Returns:
+        batched_H (jax.Array): Batched H data.
+        batched_B (jax.Array): Batched B data.
+        loader_key (jax.Array): Updated random key.
+    """
 
     n_sequences, full_sequence_length = dataset.H.shape
     n_sequence_indices, starting_points, loader_key = sample_batch_indices(
-        n_sequences, full_sequence_length, sequence_length, training_batch_size, loader_key
+        n_sequences, full_sequence_length, training_sequence_length, training_batch_size, loader_key
     )
-    batched_H, batched_B = load_batches(dataset, n_sequence_indices, starting_points, sequence_length)
+    batched_H, batched_B = load_batches(dataset, n_sequence_indices, starting_points, training_sequence_length)
 
     batched_H = jnp.squeeze(batched_H)[..., None]
     batched_B = jnp.squeeze(batched_B)[..., None]
 
     return batched_H, batched_B, loader_key
+
+
+def data_loader(dataset, training_sequence_length, training_batch_size, loader_key):
+    """TODO: Sets up a deterministic sequence of data that is iterated through.
+    Deterministic means here that is it predefined at the start of the training.
+
+    """
+    raise NotImplementedError("Deterministic data loader is not implemented yet.")
