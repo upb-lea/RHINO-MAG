95cbacc0f1b7a5a3db1a67a9ee60bbc635990f92
Hendrik Vater <hvater@mail.uni-paderborn.de>
2025-08-08 16:56:54 +0200

    moved training setup in jax from notebook to script

---
+++ mc2/runners/model_setup_jax.py
@@ -0,0 +1,54 @@
+import jax
+import jax.numpy as jnp
+import optax
+
+from mc2.features.features_jax import compute_fe_single
+from mc2.data_management import MaterialSet, load_data_into_pandas_df
+from mc2.models.model_interface import NODEwInterface, RNNwInterface
+from mc2.models.NODE import HiddenStateNeuralEulerODE
+from mc2.models.RNN import GRU
+
+
+def get_GRU_setup(material_name: str, model_key: jax.random.PRNGKey):
+    params = dict(
+        training_params=dict(
+            n_steps=10_000,
+            val_every=500,
+            tbptt_size=512,
+            past_size=10,
+            batch_size=64,
+        ),
+        model_params=dict(
+            hidden_size=8,
+            in_size=7,
+            out_size=1,
+            key=model_key,
+        ),
+        lr=1e-3,
+    )
+    optimizer = optax.adam(params["lr"])
+    model = GRU(**params["model_params"])
+
+    #####
+    # TODO: How to do this properly?
+    def featurize(norm_B_past, norm_H_past, norm_B_future, temperature):
+        past_length = norm_B_past.shape[0]
+        future_length = norm_B_future.shape[0]
+
+        featurized_B = compute_fe_single(jnp.hstack([norm_B_past, norm_B_future]), n_s=10)
+
+        return featurized_B[past_length:]
+
+    # TODO: Store normalizer objects, somewhat weird as it is?
+    data_dict = load_data_into_pandas_df(material=material_name)
+    mat_set = MaterialSet.from_pandas_dict(data_dict)
+    train_set, val_set, test_set = mat_set.split_into_train_val_test(
+        train_frac=0.7, val_frac=0.15, test_frac=0.15, seed=12
+    )
+    train_set_norm = train_set.normalize(transform_H=True, featurize=featurize)
+    normalizer = train_set_norm.normalizer
+    #####
+
+    wrapped_model = RNNwInterface(rnn=model, normalizer=normalizer, featurize=featurize)
+
+    return wrapped_model, optimizer, params
--- mc2/runners/rnn_training_jax.py
+++ mc2/runners/rnn_training_jax.py
@@ -1,28 +1,69 @@
 import argparse
 import jax
+import optax
 
-from mc2.training.jax_routine import train_recursive_nn
+from mc2.data_management import AVAILABLE_MATERIALS
+from mc2.training.jax_routine import train_model
+from mc2.runners.model_setup_jax import get_GRU_setup
+
+supported_model_types = ["GRU"]  # ["EulerNODE", "HiddenStateEulerNODE", "GRU"]
 
 
 def parse_args() -> argparse.Namespace:
     parser = argparse.ArgumentParser(description="Train recursive NNs")
-    parser.add_argument("-t", "--tag", default=None, required=False, help="an identifier/tag/comment for the trials")
+    # parser.add_argument("-t", "--tag", default=None, required=False, help="an identifier/tag/comment for the trials")
     parser.add_argument(
-        "-m",
         "--material",
         default=None,
-        required=False,
-        help="Material label to train on. Leave blank for all materials",
+        required=True,
+        help=f"Material label to train on. One of {AVAILABLE_MATERIALS}",  # TODO: is this accurate?
     )
-    parser.add_argument("-e", "--epochs", default=100, required=False, type=int, help="Number of epochs to train")
-    parser.add_argument("-d", "--debug", action="store_true", default=False, help="Run in debug mode with reduced data")
+    parser.add_argument(
+        "--model_type",
+        default=None,
+        required=True,
+        help=f"Model type to train with. One of {supported_model_types}",
+    )
+    # parser.add_argument("-e", "--epochs", default=100, required=False, type=int, help="Number of epochs to train")
+    # parser.add_argument("-d", "--debug", action="store_true", default=False, help="Run in debug mode with reduced data")
     args = parser.parse_args()
     return args
 
 
 def main():
     args = parse_args()
-    train_recursive_nn(material=args.material, n_epochs=args.epochs, debug=args.debug)
+
+    # setup
+    seed = 5
+    key = jax.random.PRNGKey(seed)
+    key, training_key, model_key = jax.random.split(key, 3)
+
+    assert (
+        args.material in AVAILABLE_MATERIALS
+    ), f"Material {args.material} is not available. Choose on of {AVAILABLE_MATERIALS}."
+
+    # TODO: params as .yaml files?
+    if args.model_type == "GRU":
+        wrapped_model, optimizer, params = get_GRU_setup(args.material, model_key)
+    else:
+        raise ValueError(f"Unknown model type: {args.model_type}. Choose on of {supported_model_types}")
+
+    # run training
+    logs, model = train_model(
+        model=wrapped_model,
+        optimizer=optimizer,
+        material_name=args.material,
+        key=training_key,
+        seed=seed,
+        **params["training_params"],
+    )
+
+    # eval
+
+    # store results
+    #
+    # store model
+    # store logs
 
 
 if __name__ == "__main__":
