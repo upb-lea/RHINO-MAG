b377c09a9ec157e8463855dcedef690316d5e890
Hendrik Vater <hvater@mail.uni-paderborn.de>
2025-08-08 16:55:44 +0200

    cleaned up training functions

--- mc2/training/jax_routine.py
+++ mc2/training/jax_routine.py
@@ -4,30 +4,16 @@ import jax.numpy as jnp
 import equinox as eqx
 import optax
 import pandas as pd
-import torch
-import json
-import random
 import logging as log
-from tqdm.notebook import trange
-from joblib import Parallel, delayed
-from typing import Dict
-from torchinfo import summary
+from tqdm import trange
 from mc2.data_management import (
-    AVAILABLE_MATERIALS,
-    book_keeping,
-    get_train_val_test_pandas_dicts,
     load_data_into_pandas_df,
-    setup_package_logging,
     MaterialSet,
     FrequencySet,
-    NormalizedFrequencySet,
 )
-from mc2.features.features_jax import add_fe
-from mc2.models.RNN import BaseRNN
 from mc2.training.data_sampling import draw_data_uniformly
 from mc2.training.optimization import make_step
 
-from IPython.display import clear_output, display
 
 DO_TRANSFORM_H = True
 H_FACTOR = 1.2
@@ -65,148 +51,6 @@ def normalize_material_set(mat_set: MaterialSet, max_d: dict[str, float], reduce
         frequency_sets=normalized_freq_sets,
         frequencies=mat_set.frequencies,
     )
-
-
-def train_recursive_nn(
-    material=None,
-    debug: bool = False,
-    n_steps: int = 100,
-    seed: int = 5,
-    tbptt_size: int = 64,
-    batch_size: int = 1024,
-):
-
-    n_steps = 3 if debug else n_steps
-
-    if material is None:
-        material = "N87"
-    data_dict = load_data_into_pandas_df(material=material)
-    mat_set = MaterialSet.from_pandas_dict(data_dict)
-
-    train_set, val_set, test_set = mat_set.split_into_train_val_test(
-        train_frac=0.7, val_frac=0.15, test_frac=0.15, seed=12
-    )
-    max_l_d = {"B": [], "H": []}
-    max_d = {"B": None, "H": None, "T": 100}  # max temperature is 100°C
-
-    for freq_set in train_set.frequency_sets:
-        max_B = jnp.abs(freq_set.B).max()
-        max_H = jnp.abs(freq_set.H).max()
-        max_l_d["B"].append(float(max_B))
-        max_l_d["H"].append(float(max_H))
-
-    for k, max_l in max_l_d.items():
-        max_v = max(max_l)
-        log.info(f"max {k} value in training set: {max_v:.2f} T or A/m or °C")
-        max_d[k] = max_v
-
-    print(max_l_d)
-    # normalize
-    train_set_norm = normalize_material_set(train_set, max_d, reduce_train=debug)
-    val_set_norm = normalize_material_set(val_set, max_d)
-    test_set_norm = normalize_material_set(test_set, max_d)
-    del train_set, val_set, test_set  # free memory
-
-    log.info(
-        f"train size: {sum(freq_set.H.shape[0] for freq_set in train_set_norm.frequency_sets)}, "
-        f"val size: {sum(freq_set.H.shape[0] for freq_set in val_set_norm.frequency_sets)}, "
-        f"test size: {sum(freq_set.H.shape[0] for freq_set in test_set_norm.frequency_sets)}"
-    )
-
-    # FREQ_CATEGORIES = jnp.array([50000.0, 80000.0, 125000.0, 200000.0, 320000.0, 500000.0, 800000.0])
-
-    def featurize(B, H, T, f):
-        # fes = add_fe(jnp.reshape(B, (1, -1)), n_s=tbptt_size)
-        return jnp.concatenate([B, T, f], axis=-1), jnp.tanh(H_FACTOR * H)  # fes[0],
-
-    @eqx.filter_jit
-    def process_freq_set(model, opt_state, key, freq_set, optimizer):
-        key, subkey = jax.random.split(key)
-        batch_H, batch_B, batch_T, _ = draw_data_uniformly(
-            freq_set,
-            training_sequence_length=tbptt_size,
-            training_batch_size=batch_size,
-            loader_key=subkey,
-        )
-        batch_f = freq_set.frequency * jnp.ones_like(batch_B) / 800_000
-
-        batch_x, batch_y = jax.vmap(featurize)(batch_B, batch_H, batch_T, batch_f)
-        loss, model, opt_state = make_step(model, batch_x, batch_y, optimizer, opt_state)
-
-        return loss, model, opt_state, key
-
-    def train_step(model, opt_state, train_set_norm, optimizer, featurize, key):
-
-        train_loss = 0.0
-        for freq_set in train_set_norm.frequency_sets:
-            loss, model, opt_state, key = process_freq_set(model, opt_state, key, freq_set, optimizer)
-            train_loss += loss / len(train_set_norm.frequencies)
-
-        return train_loss, model, opt_state
-
-    @eqx.filter_jit
-    def process_freq_set_val(freq_set, model, featurize):
-        f = freq_set.frequency / 800_000
-        B = freq_set.B[..., None]
-        H = freq_set.H[..., None]
-        T = jnp.broadcast_to(freq_set.T[:, None, None], B.shape)
-        F = f * jnp.ones_like(B)
-
-        batch_x, batch_y = jax.vmap(featurize)(B, H, T, F)
-        pred_y = jax.vmap(model)(batch_x)
-        loss = jnp.mean((pred_y - batch_y) ** 2)
-        return loss
-
-    def val_test(set, model, featurize):
-
-        val_loss = 0.0
-        for freq_set in set.frequency_sets:
-            loss = process_freq_set_val(freq_set, model, featurize)
-            val_loss += loss / len(set.frequencies)
-
-        return val_loss
-
-    key = jax.random.PRNGKey(seed)
-    logs = {
-        "material": material,
-        "loss_trends_train": [],
-        "loss_trends_val": [],
-        "start_time": pd.Timestamp.now().round(freq="s"),
-    }
-
-    hidden_size = 8
-    in_size = 3
-    out_size = 1
-    model = BaseRNN(in_size, out_size, hidden_size, key=key)
-    optimizer = optax.adam(1e-3)
-    opt_state = optimizer.init(model)
-
-    pbar = trange(n_steps, desc=f"Seed {seed}", position=seed, unit="step")
-    test_loss = val_test(test_set_norm, model, featurize)
-    log.info(f"Test loss seed {seed}: {test_loss:.3f} A/m")
-    for step in pbar:
-        train_loss = 0
-        # train_data: (N, S, features), (N, S, output_features)
-        key, subkey = jax.random.split(key)
-        train_loss, model, opt_state = train_step(model, opt_state, train_set_norm, optimizer, featurize, subkey)
-        pbar_str = f"Loss {train_loss:.2e}"
-        if step % VAL_EVERY == 0:
-            val_loss = val_test(val_set_norm, model, featurize)
-            logs["loss_trends_val"].append(val_loss)
-        pbar_str += f"| val loss {val_loss:.2e}"
-        logs["loss_trends_train"].append(train_loss)
-        pbar.set_postfix_str(pbar_str)
-
-    test_loss = val_test(test_set_norm, model, featurize)
-    log.info(f"Test loss seed {seed}: {test_loss:.3f} A/m")
-
-    logs["end_time"] = pd.Timestamp.now().round(freq="s")
-    logs["seed"] = seed
-    return logs, model
-
-
-################################################################
-################################################################
 
 
 @eqx.filter_value_and_grad
@@ -315,16 +159,15 @@ def train_model(
     model,
     optimizer,
     material_name,
-    n_steps: int = 1000,
-    val_every: int = 100,
-    seed: int = 5,
-    tbptt_size: int = 64,
-    past_size: int = 750,
-    batch_size: int = 1024,
+    key: jax.random.PRNGKey,
+    seed: int,
+    n_steps: int,
+    val_every: int,
+    tbptt_size: int,
+    past_size: int,
+    batch_size: int,
 ):
 
-    if material_name is None:
-        material_name = "N87"
     data_dict = load_data_into_pandas_df(material=material_name)
     mat_set = MaterialSet.from_pandas_dict(data_dict)
 
@@ -341,7 +184,6 @@ def train_model(
     val_set_norm = val_set.normalize(normalizer=model.normalizer, transform_H=True)
     test_set_norm = test_set.normalize(normalizer=model.normalizer, transform_H=True)
 
-    key = jax.random.PRNGKey(seed)
     logs = {
         "material": material_name,
         "loss_trends_train": [],
