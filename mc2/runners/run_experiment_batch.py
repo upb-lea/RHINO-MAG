a93c26cfd0dde3a5ed67b742e3453da4c46ba7e5
Hendrik Vater <hvater@mail.uni-paderborn.de>
2026-01-12 16:38:31 +0100

    new experiment batch for reduced feature set, gru10 default, larger kernel

--- mc2/runners/run_experiment_batch.py
+++ mc2/runners/run_experiment_batch.py
@@ -33,18 +33,39 @@ if __name__ == "__main__":
     if args.material == "A":
         epochs = 10_000
     else:
-        epochs = 1500
+        epochs = 2500
 
     if args.material == "C":
         past_size = 1
     else:
         past_size = 28
 
+    ## reduced feature set
+    train_model_jax(
+        material=args.material,
+        model_type=["GRU10"],
+        seeds=[12, 53, 66],
+        exp_name=f"reduced-features-{accuracy_tag}",
+        loss_type="adapted_RMS",
+        gpu_id=args.gpu_id,
+        epochs=epochs,
+        batch_size=512,
+        tbptt_size=156,
+        past_size=past_size,
+        time_shift=0,
+        noise_on_data=0.0,
+        tbptt_size_start=None,
+        disable_f64=args.disable_f64,
+        disable_features="reduce",
+        transform_H=False,
+        use_all_data=False,
+    )
+
     ## Default setup
     train_model_jax(
         material=args.material,
-        model_type=["GRU8"],
-        seeds=[12, 53, 66, 105, 6],
+        model_type=["GRU10"],
+        seeds=[12, 53, 66],
         exp_name=f"default{accuracy_tag}",
         loss_type="adapted_RMS",
         gpu_id=args.gpu_id,
@@ -60,12 +81,12 @@ if __name__ == "__main__":
         transform_H=False,
     )
 
-    ## Transformed
+    ## larger kernel
     train_model_jax(
         material=args.material,
-        model_type=["GRU8"],
-        seeds=[12, 53, 66, 105, 6],
-        exp_name=f"transformed{accuracy_tag}",
+        model_type=["GRU10"],
+        seeds=[12, 53, 66],
+        exp_name=f"larger-kernel-{accuracy_tag}",
         loss_type="adapted_RMS",
         gpu_id=args.gpu_id,
         epochs=epochs,
@@ -75,47 +96,69 @@ if __name__ == "__main__":
         time_shift=0,
         noise_on_data=0.0,
         tbptt_size_start=None,
+        dyn_avg_kernel_size=50.0,
         disable_f64=args.disable_f64,
         disable_features=False,
-        transform_H=True,
+        transform_H=False,
+        use_all_data=False,
     )
 
-    ## incl shift
-    train_model_jax(
-        material=args.material,
-        model_type=["GRU8"],
-        seeds=[12, 53, 66, 105, 6],
-        exp_name=f"shift{accuracy_tag}",
-        loss_type="adapted_RMS",
-        gpu_id=args.gpu_id,
-        epochs=epochs,
-        batch_size=512,
-        tbptt_size=156,
-        past_size=past_size,
-        time_shift=5,
-        noise_on_data=0.0,
-        tbptt_size_start=None,
-        disable_f64=args.disable_f64,
-        disable_features=False,
-        transform_H=True,
-    )
+    # ## Transformed
+    # train_model_jax(
+    #     material=args.material,
+    #     model_type=["GRU8"],
+    #     seeds=[12, 53, 66, 105, 6],
+    #     exp_name=f"transformed{accuracy_tag}",
+    #     loss_type="adapted_RMS",
+    #     gpu_id=args.gpu_id,
+    #     epochs=epochs,
+    #     batch_size=512,
+    #     tbptt_size=156,
+    #     past_size=past_size,
+    #     time_shift=0,
+    #     noise_on_data=0.0,
+    #     tbptt_size_start=None,
+    #     disable_f64=args.disable_f64,
+    #     disable_features=False,
+    #     transform_H=True,
+    # )
 
-    ## incl shift
-    train_model_jax(
-        material=args.material,
-        model_type=["GRU8"],
-        seeds=[12, 53, 66, 105, 6],
-        exp_name=f"long{accuracy_tag}",
-        loss_type="adapted_RMS",
-        gpu_id=args.gpu_id,
-        epochs=epochs * 2,
-        batch_size=512,
-        tbptt_size=156,
-        past_size=past_size,
-        time_shift=5,
-        noise_on_data=0.0,
-        tbptt_size_start=None,
-        disable_f64=args.disable_f64,
-        disable_features=False,
-        transform_H=True,
-    )
+    # ## incl shift
+    # train_model_jax(
+    #     material=args.material,
+    #     model_type=["GRU8"],
+    #     seeds=[12, 53, 66, 105, 6],
+    #     exp_name=f"shift{accuracy_tag}",
+    #     loss_type="adapted_RMS",
+    #     gpu_id=args.gpu_id,
+    #     epochs=epochs,
+    #     batch_size=512,
+    #     tbptt_size=156,
+    #     past_size=past_size,
+    #     time_shift=5,
+    #     noise_on_data=0.0,
+    #     tbptt_size_start=None,
+    #     disable_f64=args.disable_f64,
+    #     disable_features=False,
+    #     transform_H=True,
+    # )
+
+    # ## incl shift
+    # train_model_jax(
+    #     material=args.material,
+    #     model_type=["GRU8"],
+    #     seeds=[12, 53, 66, 105, 6],
+    #     exp_name=f"long{accuracy_tag}",
+    #     loss_type="adapted_RMS",
+    #     gpu_id=args.gpu_id,
+    #     epochs=epochs * 2,
+    #     batch_size=512,
+    #     tbptt_size=156,
+    #     past_size=past_size,
+    #     time_shift=5,
+    #     noise_on_data=0.0,
+    #     tbptt_size_start=None,
+    #     disable_f64=args.disable_f64,
+    #     disable_features=False,
+    #     transform_H=True,
+    # )
