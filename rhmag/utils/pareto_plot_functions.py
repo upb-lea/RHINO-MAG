80a3ce6dc2da57e1bfd76fa9a45ce6af288cd4eb
Hendrik Vater <hvater@mail.uni-paderborn.de>
2026-06-16 11:14:41 +0200

    pareto front plot averaged over the materials

--- rhmag/utils/pareto_plot_functions.py
+++ rhmag/utils/pareto_plot_functions.py
@@ -384,3 +384,122 @@ def visualize_pareto_single_plot(
             )
     fig.tight_layout(w_pad=0.4, h_pad=0.1)
     return fig, axs
+
+
+def visualize_pareto_front_average(
+    df,
+    df_external,
+    metrics,
+    colors,
+    labels,
+    color_others,
+    sharex,
+    sharey,
+    xlim,
+    show_median,
+    scale_log_metric=True,
+    scale_log_size=True,
+    figsize=(7.167, 7.167 / 2),
+    show_legend=True,
+):
+    df = df.copy()
+    df_external = df_external.copy()
+
+    df["color"] = df["model_type"].apply(lambda x: color_helper(x, colors, color_others))
+
+    df_external["color"] = color_others
+
+    fig, axs = plt.subplots(nrows=1, ncols=len(metrics), sharex=sharex, sharey=sharey, figsize=figsize, squeeze=False)
+
+    for i, metric in enumerate(metrics):
+        ax = axs[0, i]
+
+        xlimit = xlim[i]
+        ax.set_xlim(xlimit)
+        ax.set_ylim(5, 100_000)
+
+        target_col = f"{metric}_95th"
+
+        # Start own results
+        for color, group_df in df.groupby("color"):
+            current_color = color
+
+            avg_df = group_df.groupby("model_type").median(numeric_only=True)
+            sorted_indices = np.argsort(avg_df.n_params)
+            avg_df = avg_df.iloc[sorted_indices]
+
+            avg = avg_df[f"{metric}_95th"]
+            ax.plot(avg, avg_df["n_params"], c=current_color, alpha=0.8, marker="x", ms=3)
+        # End own results
+
+        # Start external results:
+        sns.scatterplot(
+            data=df_external,
+            x=target_col,
+            y="n_params",
+            color=color_others,
+            marker="s",
+            s=20,
+            alpha=0.8,
+            ax=ax,
+            zorder=3,
+        )
+        for _, row in df_external.iterrows():
+            ax.text(
+                row[target_col] * 1.05,
+                row["n_params"],
+                str(row["model_type"]),
+                fontsize=10,
+                alpha=1,
+                va="center",
+                color=color_others,
+                fontweight="normal",
+                clip_on=True,
+            )
+        # End external results
+
+        if scale_log_metric:
+            ax.set_xscale("log")
+        if scale_log_size:
+            ax.set_yscale("log")
+
+        unique_params = sorted(df["n_params"].unique().astype(int))
+        # ax.set_yticks(unique_params)
+        ax.yaxis.set_major_formatter(plt.ScalarFormatter())
+        ax.xaxis.set_major_formatter(plt.ScalarFormatter())
+
+        # ax.set_yticklabels(unique_params, rotation=0, fontsize=9)  # ha='right')
+        ax.tick_params(which="major", axis="y", direction="in")
+        ax.tick_params(which="both", axis="x", direction="in")
+        ax.yaxis.minorticks_off()
+        ax.xaxis.minorticks_off()
+
+        if i == 0:
+            ax.set_ylabel("\# Model params.", fontsize=9)
+
+        label_map = {"sre": "95-th percentile SRE", "nere": "95-th percentile NERE"}
+        ax.set_xlabel(label_map.get(metric, metric), fontsize=9)
+
+        ax.grid(True, which="both", ls="--", alpha=0.3)
+
+    if show_legend:
+        legend_elements = [
+            Line2D([0], [0], color=color, label=labels[model_label], markersize=5)
+            for model_label, color in colors.items()
+        ]
+        legend_elements.append(
+            Line2D([0], [0], marker="s", color="w", markerfacecolor=color_others, label="External", markersize=5)
+        )
+
+        fig.legend(
+            prop={"size": 8},
+            handles=legend_elements,
+            framealpha=0.5,
+            loc="center",
+            fancybox=True,
+            shadow=False,
+            bbox_to_anchor=(0.525, -0.05),
+            ncols=5,
+        )
+    fig.tight_layout()
+    return fig, axs
